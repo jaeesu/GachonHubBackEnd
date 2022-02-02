ef8b428378ffb872ff78fd2ae2607bc526d516e9
jaeesu <wogmlsu@gmail.com>
2022-02-02 13:02:23 +0900

    feat: heroku redis 연동

--- src/main/java/com/example/gachonhub/config/RedisConfig.java
+++ src/main/java/com/example/gachonhub/config/RedisConfig.java
@@ -1,8 +1,14 @@
 package com.example.gachonhub.config;
 
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
+import io.lettuce.core.protocol.ProtocolVersion;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -11,9 +17,22 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
     //6379, 100mb
+
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.redis.password}")
+    private String redisPassword;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory();
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(redisHost, redisPort);
+        redisStandaloneConfiguration.setPassword(redisPassword);
+
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration);
         return lettuceConnectionFactory;
     }
     @Bean
---
+++ src/test/java/com/example/gachonhub/redisTemplate/RedisCustomTemplateTest.java
@@ -0,0 +1,48 @@
+package com.example.gachonhub.redisTemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ActiveProfiles("prod")
+@SpringBootTest
+class RedisCustomTemplateTest {
+
+    @Autowired
+    private RedisCustomTemplate redisCustomTemplate;
+
+    @Autowired
+    private LettuceConnectionFactory factory;
+
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Test
+    void redisConnectionTest() {
+        String hostName = factory.getHostName();
+        int port = factory.getPort();
+
+        assertThat(hostName).isEqualTo(redisHost);
+        assertThat(port).isEqualTo(redisPort);
+    }
+
+    @Test
+    void redisTest() {
+        redisCustomTemplate.setRedisStringValue("test", "hello");
+        String test = redisCustomTemplate.getRedisStringValue("test");
+        redisCustomTemplate.deleteRedisStringValue("test");
+        String test1 = redisCustomTemplate.getRedisStringValue("test");
+
+        assertThat(test).isEqualTo("hello");
+        assertThat(test1).isNull();
+    }
+
+}
