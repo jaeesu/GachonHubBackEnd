f16fad81b1cffbdefe6d60de8447ec093a6dbd10
jaeesu <wogmlsu@gmail.com>
2022-02-18 14:15:29 +0900

    test: profile crud api 테스트

--- src/main/java/com/example/gachonhub/exception/BadRequestException.java
+++ src/main/java/com/example/gachonhub/exception/BadRequestException.java
@@ -6,8 +6,11 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class BadRequestException extends RuntimeException {
 
+    private String message;
+
     public BadRequestException(String message) {
         super(message);
+        this.message = message;
     }
 
     public BadRequestException(String message, Throwable cause) {
